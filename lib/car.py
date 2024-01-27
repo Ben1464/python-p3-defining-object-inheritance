278508faaf29a635b14ad19ecd01f7594c1c06f6
Benson Mwangi <kimaniben@gmail.com>
2024-01-27 11:55:41 +0000

    Completed asignment

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,20 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+
+    def fill_up_tank(self):
+                return "filling up!"
+
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+
+
+
+
+
+my_car=Car(wheel_size=1,wheel_number=2)
+
+print(my_car.go())
+
+print(my_car.fill_up_tank())
